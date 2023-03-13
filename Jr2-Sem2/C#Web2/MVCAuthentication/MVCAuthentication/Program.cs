31d13adffed25e8d73fc60d25e1fe1da4a8740e0
Ben-Sleurs <92521756+Ben-Sleurs@users.noreply.github.com>
2023-03-13 17:27:44 +0100

    jcom

--- Jr2-Sem2/C#Web2/MVCAuthentication/MVCAuthentication/Program.cs
+++ Jr2-Sem2/C#Web2/MVCAuthentication/MVCAuthentication/Program.cs
@@ -23,7 +23,28 @@ builder.Services.AddAuthentication()
         fbOpts.AppId = "199180349400081";
         fbOpts.AppSecret = "316d756e2ac49e51f04ac566408a10bb";
     });
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = "129879089061-muiiqf1te8fts2qooftht5rrm0q4lv9n.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+        options.SignInScheme = IdentityConstants.ExternalScheme;
 
+    });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "mvc";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
